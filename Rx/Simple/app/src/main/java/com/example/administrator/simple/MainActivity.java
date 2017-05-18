f9248240e937d942e0783c36862435f0d45058c7
jove.zhu <zmqiang@gmail.com>
2017-05-18 13:50:03 +0800

    init Rx.Simple

---
+++ Rx/Simple/app/src/main/java/com/example/administrator/simple/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.simple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getFragmentManager().findFragmentById(android.R.id.content) == null) {
+            getFragmentManager().beginTransaction()
+                    .add(android.R.id.content, new AsyncDemoFragment())
+                    .commit();
+        }
+    }
+}
